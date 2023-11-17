1915756eb93cb5da6a57bc6904b35a36637df7ca
MiguelMacias3M <133171043+MiguelMacias3M@users.noreply.github.com>
2023-11-16 19:44:49 -0600

    Archivos del formulario

---
+++ index.js
@@ -0,0 +1,8 @@
+
+
+function validar(){
+    var usuario = document.getElementById("user").value;
+    var password = document.getElementById("password").value;
+    alert(usuario);
+    alert(password);
+}
